700b505d6ae872214e7a1ab9205604409b11e7b7
Zennep <mads.bjerg@outlook.dk>
2026-01-20 13:15:44 +0100

    ny knap

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
  function onButton(){
     document.getElementById("demo").innerHTML = "Test"
 }
+ function onButton(){
+    document.getElementById("sigma").innerHTML = "Nøjjj en sigma knapp!!!"
+}
