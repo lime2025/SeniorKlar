21ca6bd302c36d839a45f1c9c39e0abf92768afb
Zennep <mads.bjerg@outlook.dk>
2026-01-23 09:05:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let click = true
+let click5 = true
  
 function onButton(){
     
@@ -13,6 +14,39 @@ function onButton(){
    
 }
 
+
 function Knap(){
+<<<<<<< HEAD
+    
+    if (click5 == true){
+        document.getElementById("sigma").innerHTML = "DEN VIRKER"
+        click5 = false
+    }
+    else {
+        document.getElementById("sigma").innerHTML = "Det er er sygt sigma"
+        click5 = true
+    }
+   
+=======
     document.getElementById("sigma").innerHTML = "Nøjjj en sigma knapp!!!"
+}
+
+let click1 = true
+
+function Button(){
+
+    if (click1 == true){
+        document.getElementById("bøsse").innerHTML = "Oliver Bille Gjerlevsen er bøsse!"
+        click1 = false
+    }
+    else {
+        document.getElementById("bøsse").innerHTML = "Hvem er bøsse?"
+        click1 = true
+    }
+
+
+<<<<<<< HEAD
+>>>>>>> d58135f693f9b961778cf434f19f2e6071585049
+=======
+>>>>>>> 8f9b6750a243959d365c0ecda03973143e596a40
 }
